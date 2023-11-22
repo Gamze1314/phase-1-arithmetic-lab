115ed7f48272e7a57f6555d266aa04b3ee13a13e
gamze <gamzekandara@gmail.com>
2023-11-22 16:17:23 -0500

    Adds Index.js

--- index.js
+++ index.js
@@ -1,2 +1,16 @@
+  let num1 = 1;
+  let num2 = 62;
+  const multiply = num1 * num2;
+  console.log(multiply);
 
-// Write your code here
+  let random = Number.parseInt(Math.random() + 1); 
+  console.log(random);
+  
+  let num3 = 16;
+  let x = 4;
+  let num4 = 4;
+  let mod = (num3 % num4) + x;
+  console.log(mod);
+
+ const max = Math.max(12,10,5,20);
+ console.log(max); 
